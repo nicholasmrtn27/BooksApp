fb26df373d09506e9f8e9a5c63f7e85dca039d4c
anjaygaming-27 <nklsmrtn02@gmail.com>
2020-11-10 23:08:01 +0700

    Initial commit

---
+++ app/src/main/java/com/vokasi/booksapp/ItemData.java
@@ -0,0 +1,8 @@
+package com.vokasi.booksapp;
+
+public class ItemData {
+    public static String itemTitle;
+    public static String itemAuthor;
+    public static String itemImage;
+    public static String itemDescription;
+}
